69bb8816a398ae500ca576cf97eeea7b926c23b4
ian <me@iany.me>
2023-04-15 10:20:57 +0800

    allow archive journals into journal/aged

--- shore/quickadd/open-journal.js
+++ shore/quickadd/open-journal.js
@@ -25,12 +25,12 @@ function makeDailyContent(date) {
   return [
     `![[${date.format("gggg-[W]ww")}#^index]]\n`,
     `# ${date.format("ddd, MMM D, YYYY")}\n`,
-    '**Kind**:: #periodic/daily',
+    "**Kind**:: #periodic/daily",
     `**Week**:: [[${date.format("gggg-[W]ww")}]]\n`,
-    '## Journal',
+    "## Journal",
     `**Journal**:: [[Journal ${yyyymmdd}]]`,
     `![[Journal ${yyyymmdd}#Journal]]`,
-    `![[Journal ${yyyymmdd}#Completed Tasks]]`
+    `![[Journal ${yyyymmdd}#Completed Tasks]]`,
   ].join("\n");
 }
 
@@ -38,9 +38,14 @@ module.exports = async ({ app }) => {
   const activeFile = app.workspace.getActiveFile();
   const date = deriveDateFromFile(activeFile) ?? moment();
   const yyyymmdd = date.format("YYYY-MM-DD");
+  const agedJournalPath = `journal/aged/Journal ${yyyymmdd}.md`;
   const journalPath = `journal/Journal ${yyyymmdd}.md`;
 
-  if (activeFile !== undefined && activeFile !== null && activeFile.path === journalPath) {
+  if (
+    activeFile !== undefined &&
+    activeFile !== null &&
+    (activeFile.path === journalPath || activeFile.path === agedJournalPath)
+  ) {
     // open daily note
     const dailyPath = `periodic/${yyyymmdd}.md`;
     let dailyFile = app.vault.getAbstractFileByPath(dailyPath);
@@ -48,6 +53,13 @@ module.exports = async ({ app }) => {
       dailyFile = await app.vault.create(dailyPath, makeDailyContent(date));
     }
     await app.workspace.getLeaf().openFile(dailyFile);
+    return;
+  }
+
+  if (await app.vault.adapter.exists(agedJournalPath)) {
+    await app.workspace
+      .getLeaf()
+      .openFile(app.vault.getAbstractFileByPath(agedJournalPath));
     return;
   }
 
